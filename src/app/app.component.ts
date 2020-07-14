5e60d37870ce3c754f05204a966ba318b804b833
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-14 10:36:06 +0700

    update sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'secret-box';
   public isLogin(){
-    return !localStorage.getItem('currentUser');
+    return !!localStorage.getItem('currentUser');
   }
 }
