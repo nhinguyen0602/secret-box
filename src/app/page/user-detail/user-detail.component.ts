679a7e20560873a4db52a26164a36caaea01854a
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-21 16:10:10 +0700

    show data storage info of user

--- src/app/page/user-detail/user-detail.component.ts
+++ src/app/page/user-detail/user-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/app/share/model/user';
 import { UsersService } from 'src/app/service/users.service';
+import { FileService } from 'src/app/service/file.service';
+import { log } from 'util';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,18 +14,31 @@ export class UserDetailComponent implements OnInit {
 
   constructor(
     public dialog: MatDialog,
+    private fileService: FileService,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private userService: UsersService
     ) { }
 
     public user: User;
+    public dataUsed: number;
 
   ngOnInit(): void {
     this.getUserDetail();
+    this.getFileOfUser();
   }
 
   getUserDetail(){
     this.userService.getUserDetail(this.data.name).subscribe(user => this.user = user);
+  }
+
+  getFileOfUser(){
+    this.fileService.getFileOfUser(this.data.name).subscribe(files => {
+      let rs = 0;
+      for (const val of files) {
+        rs += val.size;
+    }
+      this.dataUsed = rs / 1000 / this.user.membership_level.limit_storage_gigabytes * 100;
+    });
   }
 
 }
--- src/app/service/file.service.ts
+++ src/app/service/file.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
+  private url = environment.apiUrl + `/users`;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getFileOfUser(userId: number): Observable<File[]>{
+    return this.http.get<File[]>(`${this.url}/${userId}/files`).pipe(
+      map(res => res)
+    );
+  }
 }
