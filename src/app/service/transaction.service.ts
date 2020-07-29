d093c42070a13f754f487f75ababddce6a5f01d3
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-29 15:33:50 +0700

    create transaction service

---
+++ src/app/service/transaction.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+}
