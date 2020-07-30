b86f37ac2ee016792a3dfe7ffd8791c87b630ea0
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-30 16:08:13 +0700

    fix show snackbar

--- src/app/page/transaction/transaction.component.ts
+++ src/app/page/transaction/transaction.component.ts
@@ -5,22 +5,23 @@ import { Transaction } from 'src/app/share/model/transaction';
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.css']
+  // tslint:disable-next-line:object-literal-sort-keys
+  styleUrls: ['./transaction.component.css'],
 })
 export class TransactionComponent implements OnInit {
 
   constructor(
-    private transactionService: TransactionService
+    private transactionService: TransactionService,
   ) { }
 
   public transactions: Transaction[];
-  displayedColumns: string[] = ['id', 'user', 'price', 'price_code', 'created_at'];
-  ngOnInit(): void {
+  public displayedColumns: string[] = ['id', 'user', 'price', 'price_code', 'created_at'];
+  public ngOnInit(): void {
     this.getTransactions();
   }
 
-  getTransactions(){
-    this.transactionService.getAllTransaction().subscribe(transactions => this.transactions = transactions);
+  public getTransactions() {
+    this.transactionService.getAllTransaction().subscribe((transactions) => this.transactions = transactions);
   }
 
 }
