95ea9dffb2e8c782a6afbadd0948a14031072764
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-30 14:19:25 +0700

    change file server

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
- 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-app.use(express.static('/dist/secret-box'));
- 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/secret-box/index.html'));
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/secret-box'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/secret-box/index.html'));
 });
- 
-app.use(forceSSL());
- 
-app.listen(process.env.PORT || 8080);
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
