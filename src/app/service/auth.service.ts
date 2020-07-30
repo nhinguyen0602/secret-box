2fce59ddd3cf16934a65b882ebf72575d3ae3f06
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-30 13:47:53 +0700

    fixxxxxxx

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 @Injectable({
   providedIn: 'root',
 })
