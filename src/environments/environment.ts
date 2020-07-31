4d2e4c324a8270df2833508f022d8c2c933379c5
Nhi Nguyen <nhinguyen@novahub.vn>
2020-07-31 15:02:53 +0700

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl : 'http://localhost:3000/admin',
+  apiUrl : 'https://stark-headland-89728.herokuapp.com/admin',
   production: false,
 };
 
