d91915ba1de146c14f70c93b5513a728fa0533d9
Nhi Nguyen <nhinguyen@novahub.vn>
2020-08-04 15:48:34 +0700

    update UI diaglog confirm

--- src/app/component/confirm-dialog/confirm-dialog.component.ts
+++ src/app/component/confirm-dialog/confirm-dialog.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-dialog.component.css']
+  // tslint:disable-next-line:object-literal-sort-keys
+  styleUrls: ['./confirm-dialog.component.css'],
 })
 export class ConfirmDialogComponent implements OnInit {
-
-  @Input() message: string;
+  @Input() public message: string;
   // tslint:disable-next-line:no-output-native
   @Output() public submit = new EventEmitter();
   constructor(
-    public dialogRef: MatDialogRef<ConfirmDialogComponent>
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
   ) {}
 
-  ngOnInit() {
+  public ngOnInit() {
   }
 
-  onDismiss(): void {
+  public onDismiss(): void {
     this.dialogRef.close(false);
   }
 
-  public onConfirm(){
+  public onConfirm() {
     this.submit.emit();
   }
 }
