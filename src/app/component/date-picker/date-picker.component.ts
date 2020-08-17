2284c2730c641e437a84c94bf8e4488c7850bdb9
Nhi Nguyen <nhinguyen@novahub.vn>
2020-08-17 08:37:54 +0700

    Datepicker emulating a Year and month picker

--- src/app/component/date-picker/date-picker.component.ts
+++ src/app/component/date-picker/date-picker.component.ts
@@ -1,16 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepicker} from '@angular/material/datepicker';
+
+import * as _moment from 'moment';
+import {default as _rollupMoment, Moment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  // tslint:disable-next-line:object-literal-sort-keys
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    // tslint:disable-next-line:object-literal-sort-keys
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
   // tslint:disable-next-line:object-literal-sort-keys
   styleUrls: ['./date-picker.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      // tslint:disable-next-line:object-literal-sort-keys
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
-export class DatePickerComponent implements OnInit {
+export class DatePickerComponent {
+  public date = new FormControl(moment());
 
-  constructor() { }
+  public chosenYearHandler(normalizedYear: Moment) {
+    const ctrlValue = this.date.value;
+    ctrlValue.year(normalizedYear.year());
+    this.date.setValue(ctrlValue);
+  }
 
-  public ngOnInit() {
+  public chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.date.value;
+    ctrlValue.month(normalizedMonth.month());
+    this.date.setValue(ctrlValue);
+    datepicker.close();
   }
 
 }
